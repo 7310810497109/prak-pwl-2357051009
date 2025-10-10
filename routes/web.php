cf6bcd4aec379fae72df934694355d18ca6a66cd
IlyasIsLearning <151291039+7310810497109@users.noreply.github.com>
2025-10-10 20:15:38 +0700

    Selesai Langkah Praktikum Modul 5

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\MataKuliahController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,11 @@ Route::get('/user',[UserController::class, 'index']);
 Route::get('/user', [UserController::class, 'index'])->name('user.list');
 Route::get('/user/create',[UserController::class, 'create'])->name('user.create');
 Route::post('/user',[UserController::class, 'store'])->name('user.store');
+Route::resource('matakuliah', MataKuliahController::class);
+
+Route::get('/matakuliah', [MataKuliahController::class, 'index'])->name('matakuliah.index');
+Route::get('/matakuliah/create', [MataKuliahController::class, 'create'])->name('matakuliah.create');
+Route::post('/matakuliah', [MataKuliahController::class, 'store'])->name('matakuliah.store');
+Route::get('/matakuliah/{id}/edit', [MataKuliahController::class, 'edit'])->name('matakuliah.edit');
+Route::put('/matakuliah/{id}', [MataKuliahController::class, 'update'])->name('matakuliah.update');
+Route::delete('/matakuliah/{id}', [MataKuliahController::class, 'destroy'])->name('matakuliah.destroy');
