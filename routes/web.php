0a2e6bb55cd91976144df02f25dace2982c4f9e9
IlyasIsLearning <151291039+7310810497109@users.noreply.github.com>
2025-10-24 08:00:29 +0700

    pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MataKuliahController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,18 +20,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/profile',action: [ProfileController::class, 'profile']);
-Route::get('/user', [UserController::class, 'index'])->name('user.list');
-Route::get('/user/create',[UserController::class, 'create'])->name('user.create');
-Route::post('/user',[UserController::class, 'store'])->name('user.store');
-Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
-Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('matakuliah', MataKuliahController::class);
-Route::get('/matakuliah', [MataKuliahController::class, 'index'])->name('matakuliah.index');
-Route::get('/matakuliah/create', [MataKuliahController::class, 'create'])->name('matakuliah.create');
-Route::post('/matakuliah', [MataKuliahController::class, 'store'])->name('matakuliah.store');
-Route::get('/matakuliah/{id}/edit', [MataKuliahController::class, 'edit'])->name('matakuliah.edit');
-Route::put('/matakuliah/{id}', [MataKuliahController::class, 'update'])->name('matakuliah.update');
-Route::delete('/matakuliah/{id}', [MataKuliahController::class, 'destroy'])->name('matakuliah.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('matakuliah', MataKuliahController::class);
+    Route::get('/matakuliah', [MataKuliahController::class, 'index'])->name('matakuliah.index');
+    Route::get('/matakuliah/create', [MataKuliahController::class, 'create'])->name('matakuliah.create');
+    Route::post('/matakuliah', [MataKuliahController::class, 'store'])->name('matakuliah.store');
+    Route::get('/matakuliah/{id}/edit', [MataKuliahController::class, 'edit'])->name('matakuliah.edit');
+    Route::put('/matakuliah/{id}', [MataKuliahController::class, 'update'])->name('matakuliah.update');
+    Route::delete('/matakuliah/{id}', [MataKuliahController::class, 'destroy'])->name('matakuliah.destroy');
+
+});
+
+require __DIR__.'/auth.php';
